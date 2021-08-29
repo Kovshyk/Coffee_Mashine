117ccc8e6d3e5e50ec9cd9294353c4e952d84fab
Kovshyk <Asdw11wdsaqq>
2021-08-29 21:26:56 +0300

    vv

--- js/tabs.js
+++ js/tabs.js
@@ -2,14 +2,11 @@ let tabNavs = document.getElementsByClassName("nav_tab");
 let tabPanes = document.getElementsByClassName("tab-pane");
 
 for (let i = 0; i < tabNavs.length; i++) {
-
     tabNavs[i].addEventListener("click", function(e){
         e.preventDefault();
-        let activeTabAttr = e.target.getAttribute("data-tab");
-
+        let activeTabAttr = this.getAttribute("data-tab");
         for (let j = 0; j < tabNavs.length; j++) {
             let contentAttr = tabPanes[j].getAttribute("data-tab-content");
-
             if (activeTabAttr === contentAttr) {
                 tabNavs[j].classList.add("active");
                 tabPanes[j].classList.add("active");
